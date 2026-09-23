3cfeabeedf34af95e552138f380cb88dee776622
gecsel <gecsel@kkszki.hu>
2026-09-23 09:52:24 +0200

    Add project files.

---
+++ Sportolok13B/Controllers/EredmenyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sportolok13B.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EredmenyController : ControllerBase
+    {
+
+    }
+}
---
+++ Sportolok13B/Models/Eredmeny.cs
@@ -0,0 +1,12 @@
+﻿namespace Sportolok13B.Models
+{
+    public class Eredmeny
+    {
+        public int Id { get; set; }
+        public string? Competition { get; set; }
+        public string? Description { get; set; }
+        public DateTime ResultTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public int SportoloId { get; set; }
+    }
+}
